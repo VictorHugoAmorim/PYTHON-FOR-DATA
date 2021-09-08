c7deaafb70d98d5ce499e1213d47d64e923dd7a6
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2021-09-08 20:57:47 -0300

    código base corrigido
    erro na importação resolvido

--- src/PYTHON/PYTHON COM EXCEL/Conversores/Dollar_to_Real.py
+++ src/PYTHON/PYTHON COM EXCEL/Conversores/Dollar_to_Real.py
@@ -1,9 +1,9 @@
-From forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
 
 # Solicita o valor que será convertido $ -> R$
-valor = float(input("\nDigite o valor (USD) que será convertido (R$)"))
+valor = float(input("\nDigite o valor (USD) que será convertido (R$):\n"))
 
 # Converte e formata o resultado
 resultado = round(c.convert("USD", "BRL", valor), 2)
