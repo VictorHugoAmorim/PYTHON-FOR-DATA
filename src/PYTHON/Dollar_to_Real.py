b017faa94fbe9ff43e2679f2939841922645e9c4
Victor Hugo <64938014+VictorHugoAmorim@users.noreply.github.com>
2021-09-08 20:51:10 -0300

    base do código

---
+++ src/PYTHON/Dollar_to_Real.py
@@ -0,0 +1,13 @@
+From forex_python.converter import CurrencyRates
+
+c = CurrencyRates()
+
+# Solicita o valor que será convertido $ -> R$
+valor = float(input("\nDigite o valor (USD) que será convertido (R$)"))
+
+# Converte e formata o resultado
+resultado = round(c.convert("USD", "BRL", valor), 2)
+
+# Exibe o resultado
+print(f'${valor} = R${resultado}')
+
