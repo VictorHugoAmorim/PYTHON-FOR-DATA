fe65f2e0f284ea20933f91a1e85aae89542e0004
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2021-12-05 22:02:04 -0300

    update_valores

---
+++ src/PYTHON/Tuplas/juntando_listas_com_zip.py
@@ -0,0 +1,7 @@
+# Criando as listas 
+frutas = ['banana','morango','kiwi']
+cores = ['amarela','vermelho','verde']
+valores_kg = [6.99,12.50,16.33]
+
+#Unindo os valores usando o zip()
+print(list(zip(frutas,cores,valores_kg)))
